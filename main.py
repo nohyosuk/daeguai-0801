009747fefc39cdbc7fdf697e835453319b5d7540
gytjr4926 <gytjr4926@gmail.com>
2022-08-01 21:55:34 +0900

    update

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import streamlit as st
 
 view = [100, 78, 4, 30]
 
+
 st.write('# View List!!')
 
+show = st.checkbox('show raw data')
+if show == True:
+    st.write('## raw data')
+    view
 st.write('## raw data')
 view
 
@@ -12,3 +17,20 @@ st.bar_chart(view)
 
 st.write('## line chart')
 st.line_chart(view)
+
+topics = [
+    {'id':1, 'title':'html','view':100},
+    {'id':2, 'title':'css','view':50},
+    {'id':3, 'title':'js','view':140}
+]
+st.write('## raw data')
+topics
+
+st.write('## table')
+st.table(topics)
+
+import pandas as pd
+topics_df = pd.DataFrame.from_dict(topics)
+topics_df
+
+st.bar_chart(topics_df['view'])
